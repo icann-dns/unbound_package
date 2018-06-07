0e950fcdb6429c65afd7efc558936a94f3b33180
Steve Beattie <sbeattie@ubuntu.com>
2018-06-07 11:19:28 -0700

    Disable chroot by default.
    Gbp-Pq: disable_chroot_by_default.

--- smallapp/unbound-control-setup.sh
+++ smallapp/unbound-control-setup.sh
@@ -157,6 +157,6 @@ chmod o-rw $SVR_BASE.pem $SVR_BASE.key $CTL_BASE.pem $CTL_BASE.key
 rm -f request.cfg
 rm -f $CTL_BASE"_trust.pem" $SVR_BASE"_trust.pem" $SVR_BASE"_trust.srl"
 
-echo "Setup success. Certificates created. Enable in unbound.conf file to use"
+echo "Setup success. Certificates created."
 
 exit 0
