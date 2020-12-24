d59041aa5e7bab68b7330ec536c4869303006f00
Robert Edmonds <edmonds@debian.org>
2020-12-23 19:34:24 -0500

    [PATCH] Enable remote-control by default
    Gbp-Pq: 0001-Enable-remote-control-by-default.patch.

--- util/config_file.c
+++ util/config_file.c
@@ -277,7 +277,7 @@ config_create(void)
 	cfg->insecure_lan_zones = 0;
 	cfg->python_script = NULL;
 	cfg->dynlib_file = NULL;
-	cfg->remote_control_enable = 0;
+	cfg->remote_control_enable = 1; /* Debian specific */
 	cfg->control_ifs.first = NULL;
 	cfg->control_ifs.last = NULL;
 	cfg->control_port = UNBOUND_CONTROL_PORT;
