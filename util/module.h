6e560b28186e53f7df45597f310bd9f1a68284ec
Marc Deslauriers <marc.deslauriers@ubuntu.com>
2022-08-04 07:56:04 -0400

    [PATCH] - Fix the novel ghost domain issues CVE-2022-30698 and
    Gbp-Pq: CVE-2022-3069x.patch.

--- util/module.h
+++ util/module.h
@@ -610,6 +610,12 @@ struct module_qstate {
 	int no_cache_store;
 	/** whether to refetch a fresh answer on finishing this state*/
 	int need_refetch;
+	/** time when query was started. This is when the qstate is created.
+	 * This is used so that type NS data cannot be overwritten by them
+	 * expiring while the lookup is in progress, using data fetched from
+	 * those servers. By comparing expiry time with qstarttime for type NS.
+	 */
+	time_t qstarttime;
 
 	/**
 	 * Attributes of clients that share the qstate that may affect IP-based
